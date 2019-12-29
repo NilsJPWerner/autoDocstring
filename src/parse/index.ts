5526d48f63b0468872eb42d9e4c4b46a2fc4f694
Nils Werner <nils.jp.werner@gmail.com>
2019-12-29 18:19:14 +0100

    Refactor extension activation code and use CompletionItem (#97)

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -4,7 +4,7 @@ export { getDefinition } from "./get_definition";
 export { getDocstringIndentation } from "./get_docstring_indentation";
 export { getFunctionName } from "./get_function_name";
 export { guessType } from "./guess_type";
-export { isMultiLineString } from "./is_multi_line_string";
+export { validDocstringPrefix } from "./valid_docstring_prefix";
 export { parse } from "./parse";
 export { parseParameters } from "./parse_parameters";
 export { tokenizeDefinition } from "./tokenize_definition";
---
+++ src/test/parse/valid_docstring_prefix.spec.ts
@@ -0,0 +1,89 @@
+import chai = require("chai");
+import "mocha";
+
+import { validDocstringPrefix } from "../../parse";
+
+chai.config.truncateThreshold = 0;
+const expect = chai.expect;
+
+describe("validDocstringPrefix()", () => {
+    it("should return false if there are non whitespace characters preceding the triple quotes", () => {
+        const result = validDocstringPrefix(charsBeforeQuotes, 4, 10, '"""');
+
+        expect(result).to.equal(false);
+    });
+
+    it("should return false if there are characters after the triple quotes", () => {
+        const result = validDocstringPrefix(charsAfterQuotes, 4, 6, '"""');
+
+        expect(result).to.equal(false);
+    });
+
+    it("should return true if there are only white space characters preceding the triple quotes", () => {
+        const result = validDocstringPrefix(validDocstringStart, 4, 6, '"""');
+
+        expect(result).to.equal(true);
+    });
+
+    it("should use quote style to build matcher", () => {
+        const result = validDocstringPrefix(validSingleQuoteDocstringStart, 4, 6, "'''");
+
+        expect(result).to.equal(true);
+    });
+});
+
+const charsBeforeQuotes = `
+    return 3
+
+def function(param1):
+    i = """
+    try:
+        something()
+    except Error:
+        raise SomethingWentWrong
+    return 3
+
+def something_else():
+`;
+
+const charsAfterQuotes = `
+    return 3
+
+def function(param1):
+    """ abc
+    try:
+        something()
+    except Error:
+        raise SomethingWentWrong
+    return 3
+
+def something_else():
+`;
+
+const validDocstringStart = `
+    return 3
+
+def function(param1):
+    """
+    try:
+        something()
+    except Error:
+        raise SomethingWentWrong
+    return 3
+
+def something_else():
+`;
+
+const validSingleQuoteDocstringStart = `
+    return 3
+
+def function(param1):
+    '''
+    try:
+        something()
+    except Error:
+        raise SomethingWentWrong
+    return 3
+
+def something_else():
+`;
