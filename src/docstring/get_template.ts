fd817854ab65f713c6d31e9805673302ea04ae7f
Sam Hoffman <sam.hoffman@graphika.com>
2020-11-19 07:53:18 -0800

    add rst template to get_template

--- src/docstring/get_template.ts
+++ src/docstring/get_template.ts
@@ -8,6 +8,8 @@ export function getTemplate(docstringFormat: string): string {
             return getTemplateFile("sphinx.mustache");
         case "numpy":
             return getTemplateFile("numpy.mustache");
+	case "rst":
+	    return getTemplateFile("rst.mustache");
         default:
             return getTemplateFile("default.mustache");
     }
