49f309b225e96ca34a50e7a7c03c44092e1d8a28
s-weigand <s.weigand.phy@gmail.com>
2022-02-08 09:00:33 +0100

    🩹 Fixed definitionPattern to allow PEP 604 style return type hints

--- src/parse/tokenize_definition.ts
+++ src/parse/tokenize_definition.ts
@@ -1,5 +1,5 @@
 export function tokenizeDefinition(functionDefinition: string): string[] {
-    const definitionPattern = /(?:def|class)\s+\w+\s*\(([\s\S]*)\)\s*(->\s*(["']?)[\w\[\], \.]*\3)?:\s*(?:#.*)?$/;
+    const definitionPattern = /(?:def|class)\s+\w+\s*\(([\s\S]*)\)\s*(->\s*(["']?)[\w\[\], |\.]*\3)?:\s*(?:#.*)?$/;
 
     const match = definitionPattern.exec(functionDefinition);
     if (match == undefined || match[1] == undefined) {
