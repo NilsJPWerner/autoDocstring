6aff93d4d9c5d3ba4c954dae6d4cd9480181b5c3
Nils Werner <nils.jp.werner@gmail.com>
2022-02-11 10:58:27 -0500

    Update dependencies

--- src/test/integration/index.ts
+++ src/test/integration/index.ts
@@ -6,8 +6,8 @@ export function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: "bdd",
+        useColors: true,
     });
-    mocha.useColors(true);
 
     const testsRoot = path.resolve(__dirname, "..");
 
